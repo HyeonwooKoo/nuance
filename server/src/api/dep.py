95c4a87c270d5c215fb96e38106c7f30c5cf2bff
jrns1 <jrns11@snu.ac.kr>
2025-09-05 16:28:16 +0900

    feat: add dependency for user

--- server/src/api/dep.py
+++ server/src/api/dep.py
@@ -1,10 +1,20 @@
+
 from collections.abc import Generator
 from typing import Annotated
 
-from fastapi import Depends
+import jwt
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from pydantic import ValidationError
 from sqlmodel import Session
 
+from src.core import security
+from src.core.config import settings
 from src.core.db import engine
+from src.models.auth import TokenPayload
+from src.models.user import User
+from src.services import user as user_service
+
 
 def get_db() -> Generator[Session, None, None]:
     with Session(engine) as session:
@@ -12,3 +22,25 @@ def get_db() -> Generator[Session, None, None]:
 
 
 SessionDep = Annotated[Session, Depends(get_db)]
+reusable_oauth2 = OAuth2PasswordBearer(tokenUrl="/api/v1/auth/google")
+TokenDep = Annotated[str, Depends(reusable_oauth2)]
+
+
+def get_current_user(session: SessionDep, token: TokenDep) -> User:
+    try:
+        payload = jwt.decode(
+            token, settings.JWT_SECRET, algorithms=[security.ALGORITHM]
+        )
+        token_data = TokenPayload(**payload)
+    except (jwt.InvalidTokenError, ValidationError):
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Could not validate credentials.",
+        )
+    user = user_service.get_user_by_gmail(session, gmail=token_data.sub)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
+
+
+CurrentUser = Annotated[User, Depends(get_current_user)]
--- server/src/models/auth.py
+++ server/src/models/auth.py
@@ -3,3 +3,12 @@ from sqlmodel import SQLModel
 
 class AuthCode(SQLModel):
     code: str
+
+
+class Token(SQLModel):
+    access_token: str
+    token_type: str = "bearer"
+
+
+class TokenPayload(SQLModel):
+    sub: str
