b2d849a6343ffc56f91f0d032c862908bda0c814
jrns1 <jrns11@snu.ac.kr>
2025-10-27 12:53:59 +0900

    chore: adapt dummy data to new type

--- frontend/src/dummyItems.ts
+++ frontend/src/dummyItems.ts
@@ -5,6 +5,7 @@ export const dummyItems: Sentence[] = [
     id: 1,
     text: "The company's logo has become ubiquitous, appearing on everything from clothing to coffee mugs, making it one of the most recognized brands in the world.",
     word: {
+      id: 1,
       term: "ubiquitous",
       definition: "present, appearing, or found everywhere.",
       part_of_speech: "adjective",
@@ -16,6 +17,7 @@ export const dummyItems: Sentence[] = [
     id: 2,
     text: "The beauty of the cherry blossoms is ephemeral, lasting only for a short period each spring, which is why so many people travel to see them.",
     word: {
+      id: 2,
       term: "ephemeral",
       definition: "lasting for a very short time.",
       part_of_speech: "adjective",
@@ -27,6 +29,7 @@ export const dummyItems: Sentence[] = [
     id: 3,
     text: "The singer's mellifluous voice captivated the audience, as she effortlessly moved from one note to the next with a smooth and honeyed tone.",
     word: {
+      id: 3,
       term: "mellifluous",
       definition: "sweet or musical; pleasant to hear.",
       part_of_speech: "adjective",
@@ -38,6 +41,7 @@ export const dummyItems: Sentence[] = [
     id: 4,
     text: "The actress was so pulchritudinous that she was often cast as the romantic lead in films, her stunning beauty captivating audiences on the big screen.",
     word: {
+      id: 4,
       term: "pulchritudinous",
       definition: "having great physical beauty.",
       part_of_speech: "adjective",
@@ -49,6 +53,7 @@ export const dummyItems: Sentence[] = [
     id: 5,
     text: "The professor's sesquipedalian way of speaking made it difficult for the students to follow his lectures, as he often used long and obscure words.",
     word: {
+      id: 5,
       term: "sesquipedalian",
       definition: "using long words; long-winded.",
       part_of_speech: "adjective",
@@ -60,6 +65,7 @@ export const dummyItems: Sentence[] = [
     id: 6,
     text: "As the wedding ceremony concluded, the tintinnabulation of the church bells filled the air, a joyous and ringing sound that celebrated the happy occasion.",
     word: {
+      id: 6,
       term: "tintinnabulation",
       definition: "a ringing or tinkling sound.",
       part_of_speech: "noun",
@@ -71,6 +77,7 @@ export const dummyItems: Sentence[] = [
     id: 7,
     text: "The historical novel was praised for its verisimilitude, as the author had clearly done extensive research to create a believable and authentic portrayal of the past.",
     word: {
+      id: 7,
       term: "verisimilitude",
       definition: "the appearance of being true or real.",
       part_of_speech: "noun",
@@ -82,6 +89,7 @@ export const dummyItems: Sentence[] = [
     id: 8,
     text: "The rise of xenophobia in the country was a cause for concern, as it led to increased discrimination and hostility towards immigrants and foreigners.",
     word: {
+      id: 8,
       term: "xenophobia",
       definition:
         "dislike of or prejudice against people from other countries.",
@@ -94,6 +102,7 @@ export const dummyItems: Sentence[] = [
     id: 9,
     text: "The music and fashion of the 1980s perfectly captured the zeitgeist of the decade, a spirit of excess and materialism that defined the era.",
     word: {
+      id: 9,
       term: "zeitgeist",
       definition:
         "the defining spirit or mood of a particular period of history as shown by the ideas and beliefs of the time.",
@@ -106,6 +115,7 @@ export const dummyItems: Sentence[] = [
     id: 10,
     text: "The monk's life of abnegation was one of self-denial and sacrifice, as he gave up all worldly possessions to pursue a spiritual path.",
     word: {
+      id: 10,
       term: "abnegation",
       definition: "the act of renouncing or rejecting something.",
       part_of_speech: "noun",
@@ -117,6 +127,7 @@ export const dummyItems: Sentence[] = [
     id: 11,
     text: "Tired of the city, they moved to a small cottage in a bucolic setting, surrounded by rolling hills, green pastures, and the gentle sounds of nature.",
     word: {
+      id: 11,
       term: "bucolic",
       definition:
         "relating to the pleasant aspects of the countryside and country life.",
@@ -129,6 +140,7 @@ export const dummyItems: Sentence[] = [
     id: 12,
     text: "As the orchestra tuned their instruments, a cacophony of discordant sounds filled the concert hall, a chaotic mixture of strings, brass, and percussion.",
     word: {
+      id: 12,
       term: "cacophony",
       definition: "a harsh, discordant mixture of sounds.",
       part_of_speech: "noun",
@@ -140,6 +152,7 @@ export const dummyItems: Sentence[] = [
     id: 13,
     text: "The historical event is famous for the defenestration of the two royal governors, who were thrown out of a castle window by angry protesters.",
     word: {
+      id: 13,
       term: "defenestration",
       definition: "the action of throwing someone out of a window.",
       part_of_speech: "noun",
@@ -151,6 +164,7 @@ export const dummyItems: Sentence[] = [
     id: 14,
     text: "The ebullient crowd cheered and waved their flags, their cheerful and energetic mood infectious as they celebrated their team's victory.",
     word: {
+      id: 14,
       term: "ebullient",
       definition: "cheerful and full of energy.",
       part_of_speech: "adjective",
@@ -162,6 +176,7 @@ export const dummyItems: Sentence[] = [
     id: 15,
     text: "When he said he was going to run for president, it was a facetious remark, a flippant and humorous comment not meant to be taken seriously.",
     word: {
+      id: 15,
       term: "facetious",
       definition:
         "treating serious issues with deliberately inappropriate humor; flippant.",
@@ -174,6 +189,7 @@ export const dummyItems: Sentence[] = [
     id: 16,
     text: "My garrulous neighbor can talk for hours about the most trivial matters, his excessively talkative nature making it difficult to get a word in edgewise.",
     word: {
+      id: 16,
       term: "garrulous",
       definition: "excessively talkative, especially on trivial matters.",
       part_of_speech: "adjective",
@@ -185,6 +201,7 @@ export const dummyItems: Sentence[] = [
     id: 17,
     text: "The company's global hegemony was challenged by new and innovative competitors, who threatened its long-standing dominance in the market.",
     word: {
+      id: 17,
       term: "hegemony",
       definition:
         "leadership or dominance, especially by one country or social group over others.",
@@ -197,6 +214,7 @@ export const dummyItems: Sentence[] = [
     id: 18,
     text: "A true iconoclast, the artist challenged the conventions of the art world, creating works that were both shocking and thought-provoking.",
     word: {
+      id: 18,
       term: "iconoclast",
       definition: "a person who attacks cherished beliefs or institutions.",
       part_of_speech: "noun",
@@ -208,6 +226,7 @@ export const dummyItems: Sentence[] = [
     id: 19,
     text: "The juxtaposition of the old and new buildings in the city created a striking visual contrast, a blend of history and modernity.",
     word: {
+      id: 19,
       term: "juxtaposition",
       definition:
         "the fact of two things being seen or placed close together with contrasting effect.",
@@ -220,6 +239,7 @@ export const dummyItems: Sentence[] = [
     id: 20,
     text: "The employees were expected to kowtow to the CEO, their subservient behavior a clear indication of the company's hierarchical and authoritarian culture.",
     word: {
+      id: 20,
       term: "kowtow",
       definition: "act in an excessively subservient manner.",
       part_of_speech: "verb",
@@ -231,6 +251,7 @@ export const dummyItems: Sentence[] = [
     id: 21,
     text: "His limerence for his co-worker was all-consuming, a state of infatuation that made it difficult for him to focus on anything else.",
     word: {
+      id: 21,
       term: "limerence",
       definition:
         "the state of being infatuated or obsessed with another person, typically experienced involuntarily and characterized by a strong desire for reciprocation of one's feelings but not primarily for a sexual relationship.",
@@ -243,6 +264,7 @@ export const dummyItems: Sentence[] = [
     id: 22,
     text: "Despite the bitter rivalry, the winning team was magnanimous in victory, showing generosity and sportsmanship towards their defeated opponents.",
     word: {
+      id: 22,
       term: "magnanimous",
       definition:
         "very generous or forgiving, especially toward a rival or someone less powerful than oneself.",
@@ -255,6 +277,7 @@ export const dummyItems: Sentence[] = [
     id: 23,
     text: "The company was accused of nepotism after the CEO's son was hired for a high-paying position, despite his lack of experience.",
     word: {
+      id: 23,
       term: "nepotism",
       definition:
         "the practice among those with power or influence of favoring relatives or friends, especially by giving them jobs.",
@@ -267,6 +290,7 @@ export const dummyItems: Sentence[] = [
     id: 24,
     text: "The politician's speech seemed designed to obfuscate the issue, his use of jargon and vague language making it difficult to understand his true position.",
     word: {
+      id: 24,
       term: "obfuscate",
       definition: "render obscure, unclear, or unintelligible.",
       part_of_speech: "verb",
@@ -278,6 +302,7 @@ export const dummyItems: Sentence[] = [
     id: 25,
     text: "The hospice care focused on providing palliative treatment, aiming to relieve the patient's pain and improve their quality of life without curing the underlying illness.",
     word: {
+      id: 25,
       term: "palliative",
       definition:
         "(of a treatment or medicine) relieving pain or alleviating a problem without dealing with the underlying cause.",
@@ -290,6 +315,7 @@ export const dummyItems: Sentence[] = [
     id: 26,
     text: "His quixotic quest to build a flying car was met with skepticism, his idealistic but impractical vision a source of amusement for his friends.",
     word: {
+      id: 26,
       term: "quixotic",
       definition: "exceedingly idealistic; unrealistic and impractical.",
       part_of_speech: "adjective",
@@ -301,6 +327,7 @@ export const dummyItems: Sentence[] = [
     id: 27,
     text: "The politician's speech was filled with empty rhetoric, his persuasive words lacking any real substance or concrete proposals.",
     word: {
+      id: 27,
       term: "rhetoric",
       definition:
         "the art of effective or persuasive speaking or writing, especially the use of figures of speech and other compositional techniques.",
@@ -313,6 +340,7 @@ export const dummyItems: Sentence[] = [
     id: 28,
     text: "The king was surrounded by sycophants, who flattered him and agreed with everything he said in order to gain his favor.",
     word: {
+      id: 28,
       term: "sycophant",
       definition:
         "a person who acts obsequiously toward someone important in order to gain advantage.",
@@ -325,6 +353,7 @@ export const dummyItems: Sentence[] = [
     id: 29,
     text: "The old man was a taciturn farmer, who spoke only when necessary and preferred the quiet solitude of his fields to the company of others.",
     word: {
+      id: 29,
       term: "taciturn",
       definition:
         "(of a person) reserved or uncommunicative in speech; saying little.",
@@ -337,6 +366,7 @@ export const dummyItems: Sentence[] = [
     id: 30,
     text: "The car salesman's unctuous smile and ingratiating flattery made me suspicious of his motives, his oily charm a clear sign of his insincerity.",
     word: {
+      id: 30,
       term: "unctuous",
       definition:
         "(of a person) excessively or ingratiatingly flattering; oily.",
