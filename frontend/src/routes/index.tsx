6754ad3d034ff4885e7fa7bb254f663cf6f3f1db
jrns1 <jrns11@snu.ac.kr>
2025-10-27 15:30:30 +0900

    feat: call init when singed in

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,4 +1,7 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { useEffect } from 'react';
+
+import { useAuthStore } from '@/store/auth';
 
 import { VocaItem } from "../components/voca-item";
 import { useSentenceStore } from '../store/sentence';
@@ -8,11 +11,18 @@ export const Route = createFileRoute('/')({
 })
 
 function Index() {
-  const sentences = useSentenceStore((state) => state.items);
+  const user = useAuthStore((state) => state.user);
+  const items = useSentenceStore((state) => state.items);
+  const init = useSentenceStore((state) => state.actions.init);
+
+  useEffect(() => {
+    if (user)
+      init();
+  }, [user, init]);
 
   return (
     <div className="mx-auto flex max-w-screen-sm flex-col gap-4 p-4">
-      {sentences.map((sentence) => (
+      {items.map((sentence) => (
         <VocaItem
           key={sentence.id}
           sentence={sentence}
