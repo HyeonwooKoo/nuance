d0cabf5d07af75352358a50f743d5daa47a3a04a
구현우 <35356069+HyeonwooKoo@users.noreply.github.com>
2026-01-16 01:29:17 +0000

    feat: enable SpeedInsight

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,5 +1,6 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 import { Header } from '@/components/header';
 
@@ -11,6 +12,7 @@ const RootLayout = () => {
         <Outlet />
       </div>
       <TanStackRouterDevtools />
+      <SpeedInsights />
     </>
   );
 };
